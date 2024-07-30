7ca47001f15f3261ed4b8c49ca0a8da55bec25ca
Jakub Drdak <jak.drd@gmail.com>
2024-07-30 16:13:24 +0200

    Cleaning up the code

--- prompts.py
+++ prompts.py
@@ -4,12 +4,3 @@ required and preferred requirements that can be used when adjusting a client res
 a list of requirements along with information whether is requiriment required or just preffered. \
 Thoroughly analyse whether requirements are conjunction or disjunction.\
 In the case of disjunction keep all these requirements as one."""
-
-REQUIREMENT_CHECK = """You are an excelent Job Search Coach charged with analysing whether a candidate fulfills job requirements. \
-You will get a requirement and access to the client's resume to assess candidate's skills. \
-Decide whether it is necessary to further investigate into this requirement or there is enough evidence.
-If you think that it is not necessary to futher investigate, return ONLY string CONTINUE without any other text.\
-If you think that it is necessary to futher investigate, ask additional question(s) \
-to determin whether the candidant has relevant knowledge or experience. \
-You ultimate goal is to help tailor the resume to the job description. \
-Based on your evalution or a user response decide whether to continue the conversation."""
