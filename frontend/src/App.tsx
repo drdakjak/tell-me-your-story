6df9954dfb1d31091297676f5fda5a42899d728a
Jakub Drdak <jak.drd@gmail.com>
2024-09-14 13:27:41 +0200

    add cognito

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,50 @@
 import React from 'react';
 import './App.css';
 import Layout from './components/Layout';
+import { Amplify } from 'aws-amplify';
+import { withAuthenticator, Button, Heading } from '@aws-amplify/ui-react';
 
-const App: React.FC = () => {
+
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: import.meta.env.VITE_USER_POOL_ID,
+      userPoolClientId: import.meta.env.VITE_USER_POOL_CLIENT_ID,
+    },
+  },
+  API: {
+    REST: {
+      "Api": {
+        endpoint: import.meta.env.VITE_API_ENDPOINT,
+        region: import.meta.env.VITE_API_REGION,
+      },
+    },
+  }}, {
+  API: {
+    REST: {
+      headers: async () => {
+        const tokens = (await fetchAuthSession()).tokens;
+        const jwt = tokens?.idToken?.toString();
+          return {
+            "authorization": `Bearer ${jwt}`
+          };
+      }
+    }
+  }
+});
+const App: React.FC = ({ signOut }) => {
   return (
-    <div className="App">
-      <Layout />
+    // <div className="App">
+    //   <Layout />
+    // </div>
+
+    <div>
+      <h1>Thankyou for doing verification</h1>
+      <h2>My Content</h2>
+      <button onClick={signOut}>Sign out</button>
     </div>
+
   );
 };
 
-export default App;
+export default withAuthenticator(App);
