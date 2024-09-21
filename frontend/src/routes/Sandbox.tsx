fcd2ff6effcebff6b85d476c97e217f7423be4d6
Jakub Drdak <jak.drd@gmail.com>
2024-09-21 21:06:53 +0200

    delete sandbox

--- frontend/src/routes/Sandbox.tsx
+++
@@ -1,8 +0,0 @@
-const Sandbox = () => {
-    return (
-        
-            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScdh3syKmQovwEflO_XP59dc4EHCZK0PfiSaLotownO8UjHqA/viewform?embedded=true" width="640" height="1191" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
-       
-    );
-};
-export default Sandbox;
