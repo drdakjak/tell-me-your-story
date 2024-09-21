e08b50228ac28204f5923cd6d1a89c2ee229123b
Jakub Drdak <jak.drd@gmail.com>
2024-09-21 20:59:36 +0200

    update sandbox html

--- frontend/src/routes/Sandbox.tsx
+++ frontend/src/routes/Sandbox.tsx
@@ -1,8 +1,8 @@
 const Sandbox = () => {
     return (
-        <div>
-            <h1><iframe src="https://docs.google.com/forms/d/e/1FAIpQLScdh3syKmQovwEflO_XP59dc4EHCZK0PfiSaLotownO8UjHqA/viewform?embedded=true" width="640" height="1191" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe></h1>
-        </div>
+        
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScdh3syKmQovwEflO_XP59dc4EHCZK0PfiSaLotownO8UjHqA/viewform?embedded=true" width="640" height="1191" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+       
     );
 };
 export default Sandbox;
